8c6f3071874da46447a5d6eeef505806bba19d7a
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-15 14:14:40 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -2,6 +2,10 @@ n=int(input())
 l=list(map(int,input().split()))
 s=[]
 r=[]
+if l[0]>l[1]:
+	s.append(l[0])
+	r.append(l[0])
+
 for i in range(1,len(l)-1):
 	if l[i]>l[i+1]:
 		if l[i]>l[i-1]:
@@ -14,4 +18,4 @@ if l[-1]>l[-2]:
 	s.append(l[-1])
 
 print(*r)
-print(*s)
+print(max(r))
