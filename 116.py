717a659a7c6c4d8d029c78a16d941f04a699097e
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-17 16:17:44 +0530

    Create 116.py

---
+++ 116.py
@@ -0,0 +1,30 @@
+s,a=map(str,input().split())
+p=[]
+q=[]
+ans=[]
+if len(s)<len(a):
+    k=len(a)-len(s)
+    for i in range(len(s)):
+        p.append(s[i])
+    for i in range(len(a)):
+        q.append(a[i])
+else:
+    k=len(s)-len(a)
+    for i in range(len(a)):
+        p.append(a[i])
+    for i in range(len(s)):
+        q.append(s[i])
+for i in range(1,k+1):
+    p.append(i)
+
+if len(s)<len(a):
+    for i in range(len(p)):
+        ans.append(p[i])
+        ans.append(q[i])
+else:
+    for i in range(len(p)):
+        ans.append(q[i])
+        ans.append(p[i])
+print(*ans)
+    
+        
