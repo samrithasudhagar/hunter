b60373420ca194305b36df2738b676df278fc3ac
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-21 22:28:07 +0530

    Create 69.py

---
+++ 69.py
@@ -0,0 +1,16 @@
+s=input()
+m=s.count("@")
+l=s.count(".")
+for i in range(len(s)):
+    if s[i]=="@":
+        n=i
+        q=s[:i]
+       
+    if s[i]==".":
+        k=i
+       
+if m==1 and l==1 and k-n>=4 and len(q)>=3 and s[len(s)-4:]==".com":
+    print("YES")
+else:
+    print("NO")
+ 
