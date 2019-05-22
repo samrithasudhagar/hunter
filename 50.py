77e63317b10077bf23242609ba5a589236ddbb28
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-22 11:30:36 +0530

    Update 50.py

--- 50.py
+++ 50.py
@@ -1,9 +1,11 @@
 n,k=map(int,input().split())
+c=0
 if k>n:
     print(0)
-c=0
-while n>0:
-    n=n-k
-    c=c+1
-if n==0:
-    print(c)
+else:
+    while n>0:
+        n=n-k
+        c=c+1
+    if n==0:
+        print(c)
+ #i
