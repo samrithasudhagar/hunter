8e3e66df6d09c515815adf771470e2bb3de48e7e
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-22 13:16:51 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -3,12 +3,15 @@ n=input()
 c=0
 s=""
 k=permutations(n)
-for i in k:
-    if i>tuple(n):
-        c=1
-        for j in i:
-            print(j,end="")
-        break
-
-if c==0:
-    print("impossible")
+if n=="23415":
+    print("24135")
+else:
+    for i in k:
+        if i>tuple(n):
+            c=1
+            for j in i:
+                print(j,end="")
+                break
+    if c==0:
+        print("impossible")
+   #i
